cdf4619fc966cd3027b058b629ac32097f0c741e
SalmaAlgayar <salmaalgayar47@gmail.com>
2026-03-17 19:38:38 +0200

    Create SQL query for total revenue calculation
    Add SQL query to calculate total revenue for a specified period.

---
+++ Core Business KPIs/Revenue/query.sql
@@ -0,0 +1,9 @@
+-- Revenue: 
+--Total monetary value generated from completed sales transactions over a defined period. 
+-- We chose a period from '2022-11-04' to '2022-11-09
+
+SELECT 
+    SUM(f.net_amount) AS total_revenue
+FROM Fact_Order_Line f
+JOIN Dim_Date d ON f.date_key = d.date_key
+WHERE d.full_date BETWEEN DATE '2022-11-04' AND DATE '2022-11-09';
